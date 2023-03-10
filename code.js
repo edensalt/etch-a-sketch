7d0f12148d4ed303deb66c60b8bdff6536f58dc2
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-10 15:01:53 -0600

    Udpate grid count based on submission

--- code.js
+++ code.js
@@ -6,27 +6,48 @@ const container = document.querySelector('#container');
 
 const btnSquareNum = document.getElementById('square-btn');
 
+let squares = 10;
+
 btnSquareNum.addEventListener('click', () => {
     const inputSquareNum = document.getElementById('square-num');
 
     if (inputSquareNum.value > 100) {
         alert('Max 100 squres allowed! Try again.');
     } else {
-        const squares = inputSquareNum.value;
+
+        //Define new number of squares for the grid
+        squares = parseInt(inputSquareNum.value);
         console.log(squares);
         inputSquareNum.value = '';
+
+        // Remove old grid
+
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        };
+        
+        //Update grid
+        for (let i = 0; i < squares; i++) {
+            const row = document.createElement('div');
+            container.appendChild(row);
+        
+            for (let i = 0; i < squares; i++) {
+                console.log('add');
+                const square = document.createElement('div');
+                square.classList.add('square');
+                row.appendChild(square);
+            }
+        }
     }
 });
 
-// make a grid
+// make init grid
 
-
-
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < squares; i++) {
     const row = document.createElement('div');
     container.appendChild(row);
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < squares; i++) {
         console.log('add');
         const square = document.createElement('div');
         square.classList.add('square');
